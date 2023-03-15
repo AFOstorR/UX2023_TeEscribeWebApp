fea476690265959166fca744642824c242727770
AFOstorR <af.ostor10@uniandes.edu.co>
2023-03-14 23:43:09 -0500

    Creacion del esquema Front e inicio maquetacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
+import { ComponentRef, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {},
+  {}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ src/app/user/user.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserComponent } from './user.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [UserComponent, LoginComponent],
+  exports: [LoginComponent]
+
+})
+export class UserModule { }
